eb3a3a54c5b7811995049d6999e906486b8f4b7f
Infinity-ops <narmada.ambika@gmail.com>
2022-11-21 14:53:56 +0100

    Add download dataset to Model script

--- ansible/roles/g_nodes/files/SSD_docker.sh
+++ ansible/roles/g_nodes/files/SSD_docker.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
+
 # git clone
 git clone https://github.com/NVIDIA/DeepLearningExamples.git
-
-# copy download coco dataset script 
-chmod +x download_coco2017tf_dataset.sh
-cp download_coco2017tf_dataset.sh ./DeepLearningExamples/TensorFlow/Detection/SSD/
 
 # set working directory
 cd DeepLearningExamples/TensorFlow/Detection/SSD/
@@ -12,8 +9,26 @@ cd DeepLearningExamples/TensorFlow/Detection/SSD/
 # build docker
 docker build . -t nvidia_ssd
 
-# run the script
-./download_coco2017tf_dataset.sh nvidia_ssd ./data/coco2017_tfrecords ./checkpoints
+# Get COCO 2017 data sets
+if [ -z $1 ]; then echo "Docker container name is missing" && exit 1; fi
+CONTAINER=nvidia_ssd
+COCO_DIR="./data/coco2017_tfrecords"
+CHECKPOINT_DIR="./checkpoints"
+mkdir -p $COCO_DIR
+chmod 777 $COCO_DIR
+
+# Download backbone checkpoint
+mkdir -p $CHECKPOINT_DIR
+chmod 777 $CHECKPOINT_DIR
+cd $CHECKPOINT_DIR
+wget http://download.tensorflow.org/models/resnet_v1_50_2016_08_28.tar.gz
+tar -xzf resnet_v1_50_2016_08_28.tar.gz
+mkdir -p resnet_v1_50
+mv resnet_v1_50.ckpt resnet_v1_50/model.ckpt
+
+# Create TFRecords
+bash ${PWD}/models/research/object_detection/dataset_tools/download_and_preprocess_mscoco.sh \
+    ${PWD}/data/coco2017_tfrecords
 
 # run docker
-nvidia-docker run --gpus all --rm --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -v ${PWD}/data/coco2017_tfrecords:/data/coco2017_tfrecords -v ${PWD}/checkpoints:/checkpoints --ipc=host nvidia_ssd bash ./examples/SSD320_FP16_8GPU.sh /checkpoints
+docker run --gpus all --rm --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -v ${PWD}/data/coco2017_tfrecords:/data/coco2017_tfrecords -v ${PWD}/checkpoints:/checkpoints --ipc=host nvidia_ssd bash ./examples/SSD320_FP16_8GPU.sh ./checkpoints
