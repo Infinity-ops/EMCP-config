0b69eb95805ddda8c98956d6cd41a501fa56da8a
Infinity-ops <narmada.ambika@gmail.com>
2022-11-25 14:46:48 +0100

    Added Download dataset scripts

--- ansible/roles/g_nodes/files/SSD_docker.sh
+++ ansible/roles/g_nodes/files/SSD_docker.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-
 # git clone
 git clone https://github.com/NVIDIA/DeepLearningExamples.git
+
+# copy download coco dataset script 
+chmod +x download_coco2017tf_dataset.sh
+cp download_coco2017tf_dataset.sh ./DeepLearningExamples/TensorFlow/Detection/SSD/
 
 # set working directory
 cd DeepLearningExamples/TensorFlow/Detection/SSD/
@@ -9,25 +12,8 @@ cd DeepLearningExamples/TensorFlow/Detection/SSD/
 # build docker
 docker build . -t nvidia_ssd
 
-# Get COCO 2017 data sets
-CONTAINER="nvidia_ssd"
-COCO_DIR="./data/coco2017_tfrecords"
-CHECKPOINT_DIR="./checkpoints"
-mkdir -p $COCO_DIR
-chmod 777 $COCO_DIR
-
-# Download backbone checkpoint
-mkdir -p $CHECKPOINT_DIR
-chmod 777 $CHECKPOINT_DIR
-cd $CHECKPOINT_DIR
-wget http://download.tensorflow.org/models/resnet_v1_50_2016_08_28.tar.gz
-tar -xzf resnet_v1_50_2016_08_28.tar.gz
-mkdir -p resnet_v1_50
-mv resnet_v1_50.ckpt resnet_v1_50/model.ckpt
-
-# Create TFRecords
-bash ${PWD}/models/research/object_detection/dataset_tools/download_and_preprocess_mscoco.sh \
-    ${PWD}/data/coco2017_tfrecords
+# run the script
+./download_coco2017tf_dataset.sh nvidia_ssd ./data/coco2017_tfrecords ./checkpoints
 
 # run docker
-docker run --gpus all --rm --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -v ${PWD}/data/coco2017_tfrecords:/data/coco2017_tfrecords -v ${PWD}/checkpoints:/checkpoints --ipc=host nvidia_ssd bash ./examples/SSD320_FP16_8GPU.sh ./checkpoints
+nvidia-docker run --gpus all --rm --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -v ${PWD}/data/coco2017_tfrecords:/data/coco2017_tfrecords -v ${PWD}/checkpoints:/checkpoints --ipc=host nvidia_ssd bash ./examples/SSD320_FP16_8GPU.sh /checkpoints
--- ansible/roles/g_nodes/files/gpuNet_docker.sh
+++ ansible/roles/g_nodes/files/gpuNet_docker.sh
@@ -1,32 +1,20 @@
 #!/bin/sh
-
-# Git clone
+# git clone
 git clone https://github.com/NVIDIA/DeepLearningExamples.git
 
-# Set working directory
+# copy download ImageNet dataset script 
+chmod +x download_ImageNet_dataset.sh
+cp download_ImageNet_dataset.sh ./DeepLearningExamples/PyTorch/Classification/GPUNet/
+
+# set working directory
 cd DeepLearningExamples/PyTorch/Classification/GPUNet/
 
-# Create directory for logs and outputs
+# create directory for logs and outputs
 mkdir -p logs && mkdir -p output
 
-# Download the data
-mkdir data && cd data
-wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar  # get ILSVRC2012_img_val.tar (about 6.3 GB)
-wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar # get ILSVRC2012_img_train.tar (about 138 GB)
+# run the script
+./download_ImageNet_dataset.sh
 
-# Extract the training data
-mkdir train && mv ILSVRC2012_img_train.tar train/ && cd train
-tar -xvf ILSVRC2012_img_train.tar && rm -f ILSVRC2012_img_train.tar
-find . -name "*.tar" | while read NAME ; do mkdir -p "${NAME%.tar}"; tar -xvf "${NAME}" -C "${NAME%.tar}"; >
-cd ..
-
-# Extract the validation data and move images to subfolders:
-mkdir val && mv ILSVRC2012_img_val.tar val/ && cd val && tar -xvf ILSVRC2012_img_val.tar
-wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh | bash
-
-cd ..
-rm *.tar
-
-# Build and run the docker
+# build docker
 docker build -t gpunet .
-docker run --gpus all --rm --network=host --shm-size 600G --ipc=host -v ${PWD}/data:/root/data/imagenet/ gpunet ./train.sh $(nvidia-smi --list-gpus | wc -l) /root/data/imagenet/ --model gpunet_0 --sched step --decay-epochs 2.4 --decay-rate .97 --opt rmsproptf -b 192 --epochs 5 --opt-eps .001 -j 8 --warmup-lr 1e-6 --weight-decay 1e-5 --drop 0.3 --drop-connect 0.2 --model-ema --model-ema-decay 0.9999 --aa rand-m9-mstd0.5 --remode pixel --reprob 0.2 --lr .06 --num-classes 1000 --enable-distill False --crop-pct 1.0 --img-size 320 --amp
+docker run --gpus all --rm --network=host --shm-size 600G --ipc=host -v /usr/local/pytorch_GPUNET/DeepLearningExamples/PyTorch/Classification/GPUNet/data:/root/data/imagenet/ gpunet ./train.sh $(nvidia-smi --list-gpus | wc -l) /root/data/imagenet/ --model gpunet_0 --sched step --decay-epochs 2.4 --decay-rate .97 --opt rmsproptf -b 192 --epochs 5 --opt-eps .001 -j 8 --warmup-lr 1e-6 --weight-decay 1e-5 --drop 0.3 --drop-connect 0.2 --model-ema --model-ema-decay 0.9999 --aa rand-m9-mstd0.5 --remode pixel --reprob 0.2 --lr .06 --num-classes 1000 --enable-distill False --crop-pct 1.0 --img-size 320 --amp
